ae3aef12ef8203d71ee7315699aaa2efb030667d
Torque <Tarak@Torques-Mac-mini.local>
2018-01-07 12:46:38 +0530

    All the examples tried fron udacity

--- join.py
+++ join.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
 def test_run():
-    start_date='2017-01-22'
-    end_date='2017-01-26'
-    dates = pd.date_range(start_date,end_date)
+    start_date = '2010-01-22'
+    end_date = '2010-01-26'
+    dates = pd.date_range(start_date, end_date)
+    #build empty data frame for date range
     df1 = pd.DataFrame(index=dates)
+
+    #read SPY info with date indexing
+    dfSPY = pd.read_csv("SPY.csv", index_col="Date", parse_dates=True)
+    print dfSPY
+
+    #update the empty frame with SPY details.
+    df1 = df1.join(dfSPY)
     print df1
-    dfSPY = pd.read_csv("SPY.csv", index_col="Date", parse_dates=True, usecols=['Date','Adj Close'],
-            na_values=['nan'])
-#    print dfSPY
-    df1=df1.join(dfSPY, how='inner')
-    '''Drop NaN  -- non traded days '''
-   # df1=df1.dropna()
-    print df1
+
+
 
 if __name__ == "__main__":
     test_run()
